22b3db441108e996bcafa997fcc887c0b17c8ae5
Beto Silva <Beto Silva>
2016-11-02 18:19:01 -0200

    adicionando mascaramento de senha - tela login adm

--- adm.c
+++ adm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 #include "tela_main.c"
 
 #define TAM_NOME 50
@@ -10,21 +11,31 @@
 #define QTD_MUSICAS 100
 
 int main(){
-   
-    int i;
+   	int i;
     char pwAdm[TAM_SENHA] = "m@st3r2016";
-    char conf_pwAdm[TAM_SENHA];
+    char conf_pwAdm[TAM_SENHA], aux_pwAdm;
 
-    printf("Bem vindo, adm.\n");
+    printf("Bem vindo, adm.");
     
     char *pos;
     do{
         
-		printf("Digite sua senha para entrar: ");
-        fgets(conf_pwAdm, TAM_SENHA, stdin);
+		printf("\nDigite sua senha para entrar: ");
+        //fgets(conf_pwAdm, TAM_SENHA, stdin);
+        i = 0;
+    	while( (aux_pwAdm=getch()) != 13){
+    		conf_pwAdm[i] = aux_pwAdm;
+        	printf("*");
+        	i++;
+    	}		
+    	conf_pwAdm[i] = '\0';
+    	
+		fflush(stdin);
+		printf("%s\n", conf_pwAdm);
         
         if ((pos=strchr(conf_pwAdm, '\n')) != NULL)
         *pos = '\0'; 
+
         
     }while(strcmp(conf_pwAdm, pwAdm) != 0);
 
